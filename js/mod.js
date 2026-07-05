9fe9251547eea186e80315528a2d4478388bd7ba
YZHYZHYZHUZHUZH <Xachariah@163.com>
2026-07-05 17:42:49 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,13 +1,13 @@
 let modInfo = {
-	name: "The ??? Tree",
-	author: "nobody",
-	pointsName: "points",
+	name: "The OI Tree",
+	author: "AbsoluTe_disoRdeR",
+	pointsName: "skill",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
 	initialStartPoints: new Decimal (10), // Used for hard resets and new players
-	offlineLimit: 1,  // In hours
+	offlineLimit: 0,  // In hours
 }
 
 // Set your version in num and name
