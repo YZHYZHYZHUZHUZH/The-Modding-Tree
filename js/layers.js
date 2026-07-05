54b40043d728dd181bddafcb63e098e67275e8b3
YZHYZHYZHUZHUZH <Xachariah@163.com>
2026-07-05 17:50:48 +0800

    1

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,7 @@
 addLayer("p", {
     name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    name: "skill", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "sk", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,8 +9,8 @@ addLayer("p", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "skill", // Name of prestige currency
+    baseResource: "time", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -22,7 +23,14 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "s", description: "S: Reset for skills", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+    upgrades: {
+        11: {
+            title: "Bro, what is OI?",
+            description: "waste your time, but unlock the next upgrade.",
+            cost: new Decimal(1),
+        },
+    },
 })
--- js/mod.js
+++ js/mod.js
@@ -1,12 +1,12 @@
 let modInfo = {
 	name: "The OI Tree",
 	author: "AbsoluTe_disoRdeR",
-	pointsName: "skill",
+	pointsName: "time",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
+	initialStartPoints: new Decimal (1), // Used for hard resets and new players
 	offlineLimit: 0,  // In hours
 }
 
